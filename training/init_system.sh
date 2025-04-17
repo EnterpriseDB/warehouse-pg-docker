0bd874e8992ff4d91d118350dc25d5482bc72c9e
Himanshu Pandey <himanshu.pandey@enterprisedb.com>
2025-04-17 12:26:24 -0700

    Removed commented code

--- training/init_system.sh
+++ training/init_system.sh
@@ -30,73 +30,7 @@ sudo ln -sf /usr/bin/python2.7 /usr/bin/python
 # ----------------------------------------------------------------------
 sudo rm -rf /run/nologin
 
-# ## Change ownership to gpadmin 
-#sudo chown -R gpadmin.gpadmin /usr/local/greenplum-db \
-#                              /tmp/gpinitsystem_config \
-#                              /tmp/hostfile_gpinitsystem \
-#                              /tmp/gpdb-hosts 
 
-# ----------------------------------------------------------------------
-# Configure passwordless SSH access for 'gpadmin' user
-# ----------------------------------------------------------------------
-# The script sets up SSH key-based authentication for the 'gpadmin' user,
-# allowing passwordless SSH access. It generates a new SSH key pair if one
-# does not already exist, and configures the necessary permissions.
-# ----------------------------------------------------------------------
-#mkdir -p /home/gpadmin/.ssh
-#chmod 700 /home/gpadmin/.ssh
-
-#if [ ! -f /home/gpadmin/.ssh/id_rsa ]; then
-#    ssh-keygen -t rsa -b 4096 -C gpadmin -f /home/gpadmin/.ssh/id_rsa -P "" > /dev/null 2>&1
-#fi
-
-#cat /home/gpadmin/.ssh/id_rsa.pub >> /home/gpadmin/.ssh/authorized_keys
-#chmod 600 /home/gpadmin/.ssh/authorized_keys
-
-# Add the container's hostname to the known_hosts file to avoid SSH warnings
-#ssh-keyscan -t rsa mdw > /home/gpadmin/.ssh/known_hosts 2>/dev/null
-
-# Source environment variables and set MASTER_DATA_DIRECTORY
-#source /usr/local/greenplum-db/greenplum_path.sh
-#export MASTER_DATA_DIRECTORY=/data/master/gpseg-1
-#
-#Initialize multi node WarehousePG cluster
-
- #   sshpass -p "changeme@123" ssh-copy-id -o StrictHostKeyChecking=no sdw1
- #   sshpass -p "changeme@123" ssh-copy-id -o StrictHostKeyChecking=no sdw2
-    #sshpass -p "changeme@123" ssh-copy-id -o StrictHostKeyChecking=no smdw
- #   gpinitsystem -a \
- #                -c /tmp/gpinitsystem_config \
- #                -h /tmp/hostfile_gpinitsystem \
- #                --max_connections=100
-    #gpinitstandby -s smdw -a
-printf "sdw1\nsdw2\n" >> /tmp/gpdb-hosts
-
-#if [ $HOSTNAME == "mdw" ]; then
-#     ## Allow any host access the WarehousePG Cluster
-#     echo 'host all all 0.0.0.0/0 trust' >> /data/master/gpseg-1/pg_hba.conf
-#     gpstop -u
-#
-#     psql -d template1 \
-#          -c "ALTER USER gpadmin PASSWORD 'changeme@123'"
-#
-#     cat <<-'EOF'
-#EOF
-#
-#    cat <<-'EOF'
-#
-#EOF
-#
-#     echo "Current time: $(date)"
-#     source /etc/os-release
-#     echo "OS Version: ${NAME} ${VERSION}"
-#
-#     ## Set gpadmin password, display version and cluster configuration
-#     psql -P pager=off -d template1 -c "SELECT VERSION()"
-#     psql -P pager=off -d template1 -c "SELECT * FROM gp_segment_configuration ORDER BY dbid"
-#     psql -P pager=off -d template1 -c "SHOW optimizer"
-#fi
-#
 echo '
 ==========================================
 =  Welcome to the WarehousePG Training  =
